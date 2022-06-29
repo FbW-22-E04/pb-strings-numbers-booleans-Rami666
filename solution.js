16b35c890e369fdfed94ef867d560f8ec433d14d
Rami B <ramibasheer9@gmail.com>
2022-06-29 15:07:39 +0200

    apply changes

--- solution.js
+++ solution.js
@@ -1,1 +1,23 @@
 
+let str = "Javascript"
+console.log(str);
+
+
+console.log( str.charAt(1));
+console.log( str.charAt(5));
+
+let firstName = "j3for";
+let age = 27;
+let year = 2022;
+console.log(firstName + " is " + age + " in " + year);
+
+let isMarried = true;
+console.log("J3for is married: " + isMarried);
+
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log(programmingLanguage + " is fun: " + isFun);
+
+console.log( programmingLanguage.charAt(0));
+console.log( programmingLanguage.charAt(4));
+console.log( programmingLanguage.length);
